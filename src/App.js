7a78d64e3212a66a95595fd3d70217b8b937a60c
valeriapaulinalustres <valeriapaulinalustres@yahoo.com.ar>
2022-09-08 11:07:05 -0300

    app x App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './app.css'
+import './App.css'
 
 // styles
 //import './style.scss';
