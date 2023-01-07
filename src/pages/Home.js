c33e96519be121cc215c337586cc297e41ff6402
valeriapaulinalustres <valeriapaulinalustres@yahoo.com.ar>
2023-01-07 10:00:35 -0300

    responsive projects.css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -593,12 +593,12 @@ function Home() {
 
       <section className="home-container-sectionPhrase" ref={phraseHomeRef}>
         <p className="phrase fadeInHomePhrase">
-          "The <span className="wo fadeInHomePhrase">wo</span>man never knows
-          <span className="wo fadeInHomePhrase">s</span>he is capable until
+          "The <span className="wo fadeInHomePhrase">wo</span>man never knows {" "}
+          <span className="wo fadeInHomePhrase">s</span>he is capable until {" "}
           <span className="wo fadeInHomePhrase">s</span>he tries"
         </p>
       </section>
-      
+
       <section className="home-container-sectionTwo" ref={oneHomeRef}>
         <div className="about-left-container">
           <h2 className="aboutMe-title  fadeInHomeOne">About me</h2>
